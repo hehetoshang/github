18a76886f86122c577a1e1bc7aac59d9997c4f3c
houheya <127867059+hehetoshang@users.noreply.github.com>
2025-08-28 19:33:15 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,4 +1,5 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
+const zlib = require('zlib');
 
 module.exports = (req, res) => {
   let target = "https://github.com/";//your website url
@@ -20,28 +21,88 @@ module.exports = (req, res) => {
       //   "^/backend/": "/",
     },
     onProxyRes: (proxyRes, req, res) => {
+      // 复制原始响应头
+      Object.keys(proxyRes.headers).forEach(function(key) {
+        res.setHeader(key, proxyRes.headers[key]);
+      });
+
       // 检查响应是否为HTML内容
       const contentType = proxyRes.headers['content-type'];
       if (contentType && contentType.includes('text/html')) {
-        let body = '';
+        // 处理可能的压缩响应
+        const contentEncoding = proxyRes.headers['content-encoding'];
+        let body = [];
+
         proxyRes.on('data', (chunk) => {
-          body += chunk;
+          body.push(chunk);
         });
+
         proxyRes.on('end', () => {
-          // 替换HTML内容中的github.com链接为gh.houheya.us.kg
-          const modifiedBody = body
-            .replace(/https:\/\/github\.com\//g, 'https://gh.houheya.us.kg/')
-            .replace(/http:\/\/github\.com\//g, 'https://gh.houheya.us.kg/')
-            .replace(/github\.com\//g, 'gh.houheya.us.kg/');
+          body = Buffer.concat(body);
           
-          // 更新响应头中的Content-Length
+          // 解压缩响应内容
+          if (contentEncoding === 'gzip') {
+            zlib.gunzip(body, (err, decompressed) => {
+              if (!err) {
+                processBody(decompressed.toString('utf8'));
+              } else {
+                // 如果解压缩失败，尝试直接处理
+                processBody(body.toString('utf8'));
+              }
+            });
+          } else if (contentEncoding === 'deflate') {
+            zlib.inflate(body, (err, decompressed) => {
+              if (!err) {
+                processBody(decompressed.toString('utf8'));
+              } else {
+                processBody(body.toString('utf8'));
+              }
+            });
+          } else {
+            // 非压缩响应直接处理
+            processBody(body.toString('utf8'));
+          }
+        });
+
+        // 移除原始Content-Length头，因为我们要修改内容
+        delete res.getHeader('content-length');
+        // 如果有压缩头，移除它，因为我们要发送未压缩的修改后内容
+        delete res.getHeader('content-encoding');
+        
+        // 暂停原始响应流，防止默认发送
+        proxyRes.pause();
+        
+        // 处理HTML内容的函数
+        function processBody(html) {
+          // 使用更全面的正则表达式替换所有可能的github.com链接形式
+          const modifiedBody = html
+            // 替换完整URL (https://, http://)
+            .replace(/https?:\/\/github\.com\//g, 'https://gh.houheya.us.kg/')
+            // 替换相对路径URL (/开头)
+            .replace(/\/\/github\.com\//g, 'https://gh.houheya.us.kg/')
+            // 替换没有协议的URL
+            .replace(/github\.com\//g, 'gh.houheya.us.kg/')
+            // 替换href属性中的链接
+            .replace(/href=["']https?:\/\/github\.com([^"']*)["']/g, 'href="https://gh.houheya.us.kg$1"')
+            .replace(/href=["']\/\/github\.com([^"']*)["']/g, 'href="https://gh.houheya.us.kg$1"')
+            .replace(/href=["']\/(?!\/)[^"']*github\.com([^"']*)["']/g, 'href="https://gh.houheya.us.kg$1"')
+            // 替换src属性中的链接
+            .replace(/src=["']https?:\/\/github\.com([^"']*)["']/g, 'src="https://gh.houheya.us.kg$1"')
+            .replace(/src=["']\/\/github\.com([^"']*)["']/g, 'src="https://gh.houheya.us.kg$1"')
+            // 替换action属性中的链接
+            .replace(/action=["']https?:\/\/github\.com([^"']*)["']/g, 'action="https://gh.houheya.us.kg$1"')
+            .replace(/action=["']\/\/github\.com([^"']*)["']/g, 'action="https://gh.houheya.us.kg$1"');
+
+          // 设置新的Content-Length
           res.setHeader('Content-Length', Buffer.byteLength(modifiedBody));
           // 发送修改后的内容
           res.end(modifiedBody);
-        });
-        // 移除原始的Content-Length头，因为我们要修改内容
-        delete proxyRes.headers['content-length'];
+        }
+      } else {
+        // 非HTML内容直接传递
+        proxyRes.pipe(res);
       }
-    }
+    },
+    selfHandleResponse: true // 必须设置为true才能自定义响应处理
   })(req, res);
 };
